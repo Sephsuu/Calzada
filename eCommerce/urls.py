2b7016317b43b58b3b6e51eded4d5f9c28d024ab
Joseph Bataller <batallerjem208@gmail.com>
2023-12-20 13:47:09 +0800

    Additional

--- eCommerce/urls.py
+++ eCommerce/urls.py
@@ -6,4 +6,18 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('user_home/', views.user_home, name='user_home'),
+    path('show_all_products/', views.show_all_products, name='show_all_products'),
+    path('show_all_products_image/', views.show_all_products_image, name='show_all_products_image'),
+    path('show_shoe_products/', views.show_shoe_products, name='show_shoe_products'),
+    path('show_shoe_products_image/', views.show_shoe_products_image, name='show_shoe_products_image'),
+    path('show_cloth_products/', views.show_cloth_products, name='show_cloth_products'),
+    path('show_cloth_products_image/', views.show_cloth_products_image, name='show_cloth_products_image'),
+    path('show_gadget_products/', views.show_gadget_products, name='show_gadget_products'),
+    path('show_gadget_products_image/', views.show_gadget_products_image, name='show_gadget_products_image'),
+    path('show_skincare_products/', views.show_skincare_products, name='show_skincare_products'),
+    path('show_skincare_products_image/', views.show_skincare_products_image, name='show_skincare_products_image'),
+    path('show_poultry_products/', views.show_poultry_products, name='show_poultry_products'),
+    path('show_poultry_products_image/', views.show_poultry_products_image, name='show_poultry_products_image'),
+    
 ]
--- eCommerce/views.py
+++ eCommerce/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from . models import Product
+from . models import Product, User, Shoes, Clothes, Gadget, Skincare, Poultry
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
@@ -19,7 +19,7 @@ def login_user(request):
         if user is not None:
             login(request, user)
             messages.success(request, ("You have just logged in!"))
-            return redirect('admin:index')
+            return redirect('user_home')
         else:
             messages.success(request, ("Username and Password did not match!"))
             return redirect('login')
@@ -31,3 +31,68 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("Successfully logged out!"))
     return redirect('home')
+
+def user_home(request):
+    user = request.user
+    products = Product.objects.all()
+    return render(request, 'section/user_home.html', {'user': user, 'products':products})
+
+def show_all_products(request):
+    products = Product.objects.all().order_by('name')
+    messages_data = messages.get_messages(request)
+    return render(request, 'section/show_all_products.html', {'products':products, 'message_data':messages_data})
+
+def show_all_products_image(request):
+    products = Product.objects.all().order_by('name')
+    messages_data = messages.get_messages(request)
+    return render(request, 'section/show_all_products_image.html', {'products':products, 'message_data':messages_data})
+
+def show_shoe_products(request):
+    shoes = Shoes.objects.all().order_by('name')
+    messages_data = messages.get_messages(request)
+    return render(request, 'section/show_shoe_products.html', {'shoes':shoes, 'message_data':messages_data})
+
+def show_shoe_products_image(request):
+    shoes = Shoes.objects.all().order_by('name')
+    messages_data = messages.get_messages(request)
+    return render(request, 'section/show_shoe_products_image.html', {'shoes':shoes, 'message_data':messages_data})
+
+def show_cloth_products(request):
+    clothes = Clothes.objects.all().order_by('name')
+    messages_data = messages.get_messages(request)
+    return render(request, 'section/show_cloth_products.html', {'clothes':clothes, 'message_data':messages_data})
+
+def show_cloth_products_image(request):
+    clothes = Clothes.objects.all().order_by('name')
+    messages_data = messages.get_messages(request)
+    return render(request, 'section/show_cloth_products_image.html', {'clothes':clothes, 'message_data':messages_data})
+
+def show_gadget_products(request):
+    gadgets = Gadget.objects.all().order_by('name')
+    messages_data = messages.get_messages(request)
+    return render(request, 'section/show_gadget_products.html', {'gadgets':gadgets, 'message_data':messages_data})
+
+def show_gadget_products_image(request):
+    gadgets = Gadget.objects.all().order_by('name')
+    messages_data = messages.get_messages(request)
+    return render(request, 'section/show_gadget_products_image.html', {'gadgets':gadgets, 'message_data':messages_data})
+
+def show_skincare_products(request):
+    skincares = Skincare.objects.all().order_by('name')
+    messages_data = messages.get_messages(request)
+    return render(request, 'section/show_skincare_products.html', {'skincares':skincares, 'message_data':messages_data})
+
+def show_skincare_products_image(request):
+    skincares = Skincare.objects.all().order_by('name')
+    messages_data = messages.get_messages(request)
+    return render(request, 'section/show_skincare_products_image.html', {'skincares':skincares, 'message_data':messages_data})
+
+def show_poultry_products(request):
+    poultries = Poultry.objects.all().order_by('name')
+    messages_data = messages.get_messages(request)
+    return render(request, 'section/show_poultry_products.html', {'poultries':poultries, 'message_data':messages_data})
+
+def show_poultry_products_image(request):
+    poultries = Poultry.objects.all().order_by('name')
+    messages_data = messages.get_messages(request)
+    return render(request, 'section/show_poultry_products_image.html', {'poultries':poultries, 'message_data':messages_data})
